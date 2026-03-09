043e16fdd0b1b97688a63f1bf887ee83c08db131
gaganas1233 <sgagana713@gmail.com>
2026-03-09 22:18:02 +0530

    Render deployment fix

--- app/model_loader.py
+++ app/model_loader.py
@@ -1,13 +1,10 @@
 import numpy as np
 import pandas as pd
-from sentence_transformers import SentenceTransformer
 
-print("Loading model and data...")
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
+print("Loading embeddings and dataset...")
 
 product_embeddings = np.load("embeddings/product_embeddings_v1.npy")
 
 products_df = pd.read_csv("data/cleaned_products.csv")
 
-print("Model and data loaded.")
+print("Data loaded successfully.")
--- app/search.py
+++ app/search.py
@@ -1,35 +1,21 @@
 import numpy as np
-import pandas as pd
 from sentence_transformers import SentenceTransformer
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from app.model_loader import product_embeddings, products_df
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
-
-embeddings = np.load(os.path.join(BASE_DIR, "embeddings", "product_embeddings_v1.npy"))
-
-products_df = pd.read_csv(os.path.join(BASE_DIR, "data", "cleaned_products.csv"))
-
-# remove NaN values (important for JSON)
-products_df = products_df.fillna("")
-
 
 def search_products(query, top_k=10):
 
     query_embedding = model.encode([query], normalize_embeddings=True)
 
-    similarities = np.dot(embeddings, query_embedding.T).flatten()
+    similarities = np.dot(product_embeddings, query_embedding.T).flatten()
 
     top_indices = similarities.argsort()[-top_k:][::-1]
 
     results = products_df.iloc[top_indices]
 
-    # display only required fields
     results = results[
-        ["name", "category_source","discount_price"]
+        ["name", "main_category", "sub_category", "discount_price", "image", "link"]
     ]
 
-    results = results.replace([np.inf, -np.inf], "").fillna("")
-
-    return results.to_dict(orient="records")
+    return results.fillna("").to_dict(orient="records")
